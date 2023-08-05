6be08acad527e7e591b0178e83601bec8304b56f
nabarazz <nabarazz01@outlook.com>
2023-08-05 15:36:32 +0545

    email and password validate

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,41 +1,57 @@
-import { useState } from 'react';
-import Alert from '@mui/material/Alert';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Snackbar from '@mui/material/Snackbar';
-import Typography from '@mui/material/Typography';
-import logo from '../../assets/logo.svg';
-
+import { useState } from "react";
+import Alert from "@mui/material/Alert";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Snackbar from "@mui/material/Snackbar";
+import Typography from "@mui/material/Typography";
+import logo from "../../assets/logo.svg";
+import emailValidator from "email-validator";
 
 export default function LoginForm() {
   const [showAlert, setShowAlert] = useState(false);
   const validateForm = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const data = new FormData(event.currentTarget);
-    const email = data.get('email');
-    const password = data.get('password');
+    const email = data.get("email");
+    const password = data.get("password");
 
     // Add validation code here
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const isValidEmail = emailValidator.validate(email);
+    const isValidPassword = passwordRegex.test(password);
 
-  }
+    switch (true) {
+      case !isValidEmail:
+        setShowAlert("invalid email");
+        break;
+
+      case !isValidPassword:
+        setShowAlert("invalid password")
+        break;
+
+      default:
+        setShowAlert("sucess")
+    }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
     validateForm(event);
-    setShowAlert("Login Successful");
+    // setShowAlert("Login Successful");
   };
 
   return (
     <>
-      {showAlert &&
+      {showAlert && (
         <Snackbar
           open={showAlert}
           autoHideDuration={6000}
@@ -44,19 +60,21 @@ export default function LoginForm() {
         >
           <Alert>{showAlert}</Alert>
         </Snackbar>
-      }
+      )}
       <Grid
         item
         xs={false}
         sm={4}
         md={7}
         sx={{
-          backgroundImage: 'url(https://source.unsplash.com/random)',
-          backgroundRepeat: 'no-repeat',
+          backgroundImage: "url(https://source.unsplash.com/random)",
+          backgroundRepeat: "no-repeat",
           backgroundColor: (t) =>
-            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
+            t.palette.mode === "light"
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "center",
         }}
       />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -64,20 +82,27 @@ export default function LoginForm() {
           sx={{
             my: 8,
             mx: 4,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Box sx={{
-            my: 2
-          }}>
+          <Box
+            sx={{
+              my: 2,
+            }}
+          >
             <img src={logo} width="147" alt="harrison.ai" />
           </Box>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
